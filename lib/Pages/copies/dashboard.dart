a2ff81543de8810000dc76c1389bc937d8c49226
zphoenixz <ing.ramiro.valdez.a@gmail.com>
2018-11-19 14:12:39 -0400

    Conecction with Backend added

--- lib/Pages/copies/dashboard.dart
+++ lib/Pages/copies/dashboard.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/animation.dart';
 import 'dart:math';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+import 'package:url_launcher/url_launcher.dart';
 
 class DashboardPage extends StatefulWidget {
   // final Color color;
@@ -13,13 +17,18 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage>
-    with SingleTickerProviderStateMixin, AutomaticKeepAliveClientMixin<DashboardPage> {
+    with
+        SingleTickerProviderStateMixin,
+        AutomaticKeepAliveClientMixin<DashboardPage> {
   Animation<double> animation;
   AnimationController controller;
 
   List colors = [Colors.red, Colors.green, Colors.yellow];
   Random random = new Random();
   int index = 0;
+  String backend = "https://fotoycopia-backend.herokuapp.com";
+  List notTrahedData;
+  Future<void> _launched;
 
   @override
   bool get wantKeepAlive => true;
@@ -27,6 +36,7 @@ class _DashboardPageState extends State<DashboardPage>
   @override
   void initState() {
     super.initState();
+    this._getAllNotTrashed();
     controller = new AnimationController(
         duration: new Duration(milliseconds: 600), vsync: this);
     animation =
@@ -45,12 +55,43 @@ class _DashboardPageState extends State<DashboardPage>
     super.dispose();
   }
 
+  Future<String> _getAllNotTrashed() async {
+    String finalUrl = backend + "/get_all_not_trashed";
+    var res = await http
+        .get(Uri.encodeFull(finalUrl), headers: {"Accept": "application/json"});
+    print("==================");
+    print(res);
+    print("==================");
+    setState(() {
+      notTrahedData = json.decode(res.body);
+      print(notTrahedData);
+      print(notTrahedData[1]);
+    });
+    print("==================");
+
+    return "Success!";
+  }
+
+  Future<void> _launchInWebViewWithJavaScript(String url) async {
+    if (await canLaunch(url)) {
+      await launch(
+        url,
+        forceSafariVC: true,
+        forceWebView: true,
+        enableJavaScript: true,
+      );
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
 
     List<Widget> items = new List();
+    
 
     void changeIndex() {
       setState(() => index = random.nextInt(3));
@@ -69,37 +110,61 @@ class _DashboardPageState extends State<DashboardPage>
       );
     }
 
-    Widget _item(int index) {
-      return CircleAvatar(
-        // child: Image.asset(
-        //   "assets/Icons/male_user.png",
-        // ),
-        radius: screenHeight * 0.06,
-        backgroundColor: colors[index],
+    Widget _item(int index, int number, String webViewLink) {
+      return GestureDetector(
+        onTap: () {
+          String toLaunch = webViewLink;
+          print("tapped number: " + number.toString());
+          setState(() {
+            _launched = _launchInWebViewWithJavaScript(toLaunch);
+          });
+        },
+        child: CircleAvatar(
+          // child: Image.asset(
+          //   "assets/Icons/male_user.png",
+          // ),
+          radius: screenHeight * 0.06,
+          backgroundColor: colors[index],
+        ),
       );
     }
 
     void _cloudItems() {
-      for (int i = 0; i < 10; i++) {
+      
+      for (int i = 0; i < notTrahedData.length; i++) {
         changeIndex();
+        print(notTrahedData[i]['name'] + " - " + notTrahedData[i]['webViewLink']);
         setState(() {
-          items.add(_item(index));   
+          items.add(_item(index, i, notTrahedData[i]['webViewLink']));
         });
-        
+
         // items.add(SizedBox(width:screenWidth * 0.05));
       }
     }
+    // Este widget luego escuchara a la bd en FIRESTORE y no la de python
+    // Widget _buildArrayData(){
+
+    //   return StreamBuilder(
+    //     stream: ,
+    //     builder: (){
+
+    //       return widget;
+    //     },
+    //   )
+    // }
 
     Widget _buildCloudItems() {
       _cloudItems();
       return Container(
         padding: new EdgeInsets.only(top: 10.0, left: 10.0),
-        child: Wrap(
-          runSpacing: 20.0,
-          spacing: 10.0,
-          crossAxisAlignment: WrapCrossAlignment.end,
-          direction: Axis.horizontal,
-          children: items,
+        child: SingleChildScrollView(
+          child: Wrap(
+            runSpacing: 20.0,
+            spacing: 10.0,
+            crossAxisAlignment: WrapCrossAlignment.end,
+            direction: Axis.horizontal,
+            children: items,
+          ),
         ),
       );
     }
